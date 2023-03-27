6a4ee2cfcfd296db9d289f01c144ea5876d792f1
Henrique Pinheiro <henriquepinheiroxavier@gmail.com>
2023-03-27 16:22:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+
+function openMenu(event) {
+  menuList.classList.toggle('active');
+  menuButton.classList.toggle('active');
+}
+
+menuButton.addEventListener('click', openMenu);
